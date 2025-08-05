5f1ac9932c6ec215fb54aaf7b8141b979af92d88
AUKYJL <aukyjl@gmail.com>
2025-08-05 21:11:25 +0500

    chore: fix games filter label

--- src/pages/games/gamesList/ui/gamesFilter/gamesFilter.tsx
+++ src/pages/games/gamesList/ui/gamesFilter/gamesFilter.tsx
@@ -3,7 +3,7 @@ import { TextInput } from "react-admin";
 import { USERS_FIELDS_LABELS } from "@/pages/users/const";
 
 export const GamesFilter = [
-  <TextInput source="id" label="id вывода" />,
+  <TextInput source="id" label="id игры" />,
   <TextInput source="tgId" label={USERS_FIELDS_LABELS.TG_ID} />,
   <TextInput source="username" label={USERS_FIELDS_LABELS.USERNAME} />,
 ];
